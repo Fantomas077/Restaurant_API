3baa91ceb9d80816a048a79b3617657ff0ff291b
Fantomas077 <nheltonn@gmail.com>
2026-08-05 19:21:56 +0200

    Add Dto , creating ressource and Model Validations

---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurant.API.Controllers
+{
+    [Route("api/restaurants")]
+    [ApiController]
+    public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllRestaurant()
+        {
+            var restaurant = await restaurantsService.GetAllRestaurants();
+            return Ok(restaurant);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRestaurantById([FromRoute] int id)
+        {
+            var restaurant = await restaurantsService.GetById(id);
+            if (restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            
+            var restaurant = await restaurantsService.Create(dto);
+            return Ok(restaurant);
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 using Microsoft.EntityFrameworkCore;
+using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Application.Restaurants;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.IRepositories;
 using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Defaultconnection")));
+builder.Services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+builder.Services.AddScoped<IRestaurantsService, RestaurantsService>();
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile<RestaurantsProfile>();
+    cfg.AddProfile<DishesProfile>();
+});
 
 var app = builder.Build();
 
@@ -17,7 +30,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
